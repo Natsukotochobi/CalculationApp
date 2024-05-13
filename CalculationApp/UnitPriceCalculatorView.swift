f001a0e1a1f79838ca2bf1d740698b235a4f227b
Natsukotochobi <makumaku2014@gmail.com>
2024-05-13 11:02:25 +0900

    単価計算のビューを作成

--- CalculationApp/UnitPriceCalculatorView.swift
+++ CalculationApp/UnitPriceCalculatorView.swift
@@ -8,13 +8,44 @@
 import SwiftUI
 
 struct UnitPriceCalculatorView: View {
+    @State var totalPrice: String = "" //合計金額
+    @State var quantity: String = "" //何個か
+    
     var body: some View {
         ZStack {
-            Color.blue  // 背景色
-            Text("Page1")
-        } // ZStack
-    }
-}
+            Color(red: 0.75, green: 1.0, blue: 0.8, opacity: 0.6)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("単価計算します")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                
+                HStack {
+                    Text("合計金額：").padding(.horizontal, 0)
+                    TextField("0", text: $totalPrice)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.numberPad)
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                } // HStack
+                .frame(width: 200)
+                
+                HStack {
+                    Text("個数：").padding(.horizontal,0)
+                    TextField("0", text: $quantity)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.numberPad)
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                } // HStack
+                .frame(width: 200)
+                .padding()
+                
+                Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+                    .padding(.horizontal,0)
+                
+            } // VStack
+        } //ZStack
+    } // body
+} // UnitPriceCalculatorView
 
 #Preview {
     UnitPriceCalculatorView()
